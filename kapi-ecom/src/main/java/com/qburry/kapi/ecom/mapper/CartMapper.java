d576ff63b3d7cfddfc2bd4a59130ddf84ef54279
Kevin Azangue <kevinromuald@gmail.com>
2024-03-10 20:49:13 +0100

    fix warning mapstruct not mapped and login to docker hub

--- kapi-ecom/src/main/java/com/qburry/kapi/ecom/mapper/CartMapper.java
+++ kapi-ecom/src/main/java/com/qburry/kapi/ecom/mapper/CartMapper.java
@@ -1,11 +1,12 @@
 package com.qburry.kapi.ecom.mapper;
 
+import com.qburry.kapi.commons.MapperConfiguration;
 import com.qburry.kapi.ecom.dto.Cart;
 import com.qburry.kapi.ecom.model.CartEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = CartItemMapper.class)
+@Mapper(componentModel = "spring", config = MapperConfiguration.class, uses = CartItemMapper.class)
 public interface CartMapper {
     @Mapping(target = "user.id", source = "userId")
     CartEntity toEntity(Cart cart);
--- kapi-ecom/src/main/java/com/qburry/kapi/ecom/mapper/ProductMapper.java
+++ kapi-ecom/src/main/java/com/qburry/kapi/ecom/mapper/ProductMapper.java
@@ -1,5 +1,6 @@
 package com.qburry.kapi.ecom.mapper;
 
+import com.qburry.kapi.commons.MapperConfiguration;
 import com.qburry.kapi.ecom.dto.Product;
 import com.qburry.kapi.ecom.model.ProductEntity;
 import org.mapstruct.Mapper;
@@ -7,11 +8,13 @@ import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = CategoryMapper.class)
+@Mapper(componentModel = "spring", config = MapperConfiguration.class, uses = CategoryMapper.class)
 public interface ProductMapper {
 
     @Mapping(target = "category.children", ignore = true)
     Product toProduct(ProductEntity entity);
+
     List<Product> toProducts(List<ProductEntity> entities);
+
     ProductEntity toEntity(Product product);
 }
